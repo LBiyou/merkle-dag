1079f75f97721f68f7e8b2b195d91b8bc94997ce
biyou <3071709373@qq.com>
2024-03-13 22:33:19 +0800

    the first experiment

--- kvstore.go
+++ kvstore.go
@@ -1,8 +1,16 @@
 package merkledag
 
+// KVStore 接口定义了键值存储器的基本操作
 type KVStore interface {
+	// Has 方法用于检查存储器中是否存在指定键的值
 	Has(key []byte) (bool, error)
+
+	// Put 方法用于将键值对存储到存储器中
 	Put(key, value []byte) error
+
+	// Get 方法用于从存储器中获取指定键对应的值
 	Get(key []byte) ([]byte, error)
+
+	// Delete 方法用于删除存储器中指定键的值
 	Delete(key []byte) error
 }
